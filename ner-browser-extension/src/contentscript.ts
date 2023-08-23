c67b45a079a1633730c3a5f0648bfba45109119c
Bennett Wu <57691028+bwu1324@users.noreply.github.com>
2023-08-22 23:15:22 -0500

    add loading spinner and prompt

--- ner-browser-extension/src/contentscript.ts
+++ ner-browser-extension/src/contentscript.ts
@@ -3,6 +3,11 @@ import { Readability } from '@mozilla/readability';
 const NER_URL = 'http://localhost:8080/ner';
 
 async function main() {
+  promptNER();
+}
+
+async function startNER() {
+  showLoadingReader();
   await enableReader();
 
   const text_tags = ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
@@ -10,12 +15,38 @@ async function main() {
   for (const page of pages) {
     for (const tag of text_tags) {
       const text_element = page.getElementsByTagName(tag);
-
       for (const element of text_element) {
         await runNER(element);
       }
     }
   }
+
+  closePrompt();
+}
+
+function promptNER() {
+  const prompt = document.createElement('div');
+  prompt.id = 'ner-prompt';
+  prompt.innerHTML = `
+  <h3>Named Entity Recognition</h3>
+  <button class="ner-button" style="background-color: #96C9DC" id="ner-run-button">Run</button>
+  <button class="ner-button" style="background-color: #F06C9B" id="ner-close-button">Close</button>
+  `;
+
+  document.body.appendChild(prompt);
+
+  document.getElementById('ner-run-button').onclick = startNER;
+  document.getElementById('ner-close-button').onclick = closePrompt;
+}
+
+function closePrompt() {
+  document.getElementById('ner-prompt').remove();
+}
+
+function showLoadingReader() {
+  const prompt = document.getElementById('ner-prompt');
+
+  prompt.innerHTML = '<p>Creating Reader View...</p><p class="ner-loading"></p>';
 }
 
 async function enableReader() {
@@ -23,7 +54,18 @@ async function enableReader() {
   const reader = new Readability(documentClone as Document);
   const article = reader.parse();
 
-  const doc = new DOMParser().parseFromString(article.content, 'text/html').body;
+  const doc = new DOMParser().parseFromString(
+    `
+    <div id="ner-container">
+      ${article.content}
+      <div id="ner-prompt">
+        <p>Running Named Entity Recognition...</p>
+        <p class="ner-loading"></p>
+      </div>
+    </div>
+    `,
+    'text/html',
+  ).body;
   document.body.replaceWith(doc);
 }
 
